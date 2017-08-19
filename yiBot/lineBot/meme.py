edaf4eca4d021efa38c0ca26ebc8751492548e2a
abomyi <abomyi@gmail.om>
2017-08-20 01:04:17 +0800

    找梗圖

---
+++ yiBot/lineBot/meme.py
@@ -0,0 +1,17 @@
+import random
+
+from bs4 import BeautifulSoup
+import requests
+
+
+def findMeme(keyword):
+    googleURL = 'https://www.google.com.tw/search?q={0}&safe=off&source=lnms&tbm=isch'.format(keyword)
+    
+    result = requests.get(googleURL)
+    
+    result = BeautifulSoup(result.text, 'html.parser')
+    imgs = result.select('body img', limit=5)    #限制5筆，但是返回8筆？？？
+    
+    imgResult = [img['src'] for img in imgs]
+    
+    return random.choice(imgResult)
--- yiBot/lineBot/views.py
+++ yiBot/lineBot/views.py
@@ -5,11 +5,12 @@ from linebot.api import LineBotApi
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models.events import MessageEvent
 from linebot.models.messages import TextMessage
-from linebot.models.send_messages import TextSendMessage
+from linebot.models.send_messages import TextSendMessage, ImageSendMessage
 from linebot.webhook import WebhookParser
 
 from yiBot.settings import LINE_CHANNEL_ACCESS_TOKEN, LINE_CHANNEL_SECRET
 from lineBot.weatherApi import weatherApi
+from lineBot.meme import findMeme
 
 try:
     # 在local端沒有line的各項資料（channel access token & secret key），故本機端運行會直接卡死在這裡
@@ -45,10 +46,18 @@ def lineBot(request):
                     continue
                 print(response)
                 
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text=response)
-                )
+                try:
+                    imgURL = findMeme(response)
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        ImageSendMessage(original_content_url=imgURL,
+                                         preview_image_url=imgURL)
+                    )
+                except:
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text=response)
+                    )
                 
     return HttpResponse()
     

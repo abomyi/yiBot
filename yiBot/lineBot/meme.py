a782e43d58521149e6177a4d634c2652d0ae7c86
abomyi <abomyi@gmail.com>
2017-08-22 21:23:21 +0800

    bug fix

--- yiBot/lineBot/meme.py
+++ yiBot/lineBot/meme.py
@@ -12,11 +12,20 @@ def findMeme(keyword):
     
     result = BeautifulSoup(res.text, 'html.parser')
     
-    images = [json.loads(div.text)["ou"] for div in result.find_all("div",{"class":"rg_meta"})[:5]]
-    image = random.choice(images)
+#     images = [json.loads(div.text)["ou"] for div in result.find_all("div",{"class":"rg_meta"})[:5]]
+    images = []
+    for div in result.find_all('div', {'class':'rg_meta'})[:10]:
+        image = json.loads(div.text)["ou"]
+        if image.find('https:') != -1:
+            images.append(image)
+        
+        if len(images) >= 5:
+            # 取前五張圖片(熱門搜尋or點擊)即可
+            break
+        
+#     if image.find('https:') < 0:
+#         # Linebot 僅接受 https之圖像網址，故在此強制取代為https (很糟糕的方法，但是實用)
+#         # 後續更新: 不實用，因為部分網站強塞https會產生「此連結網站並不安全...確定要進入此網站?」之狀況，然而Linebot無法處理此情形，故會回傳一個永遠無法載入的圖片給客戶端
+#         image = image.replace('http', 'https')
     
-    if image.find('https:') < 0:
-        # Linebot 僅接受 https之圖像網址，故在此強制取代為https (很糟糕的方法，但是實用)
-        image = image.replace('http', 'https')
-    
-    return image
+    return random.choice(images)

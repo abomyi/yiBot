066ec7bc97db394afa3c6cc3271650170881f2bc
abomyi <abomyi@gmail.com>
2017-08-22 21:24:33 +0800

    優化

--- yiBot/lineBot/meme.py
+++ yiBot/lineBot/meme.py
@@ -7,15 +7,15 @@ import requests
 
 def findMeme(keyword):
     googleURL = 'https://www.google.com.tw/search?q={0}&safe=active&source=lnms&tbm=isch'.format(keyword)
-    header = {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+    header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36'}
     res = requests.get(googleURL, headers=header)
     
     result = BeautifulSoup(res.text, 'html.parser')
     
-#     images = [json.loads(div.text)["ou"] for div in result.find_all("div",{"class":"rg_meta"})[:5]]
+#     images = [json.loads(div.text)['ou'] for div in result.find_all('div',{'class':'rg_meta'})[:5]]
     images = []
     for div in result.find_all('div', {'class':'rg_meta'})[:10]:
-        image = json.loads(div.text)["ou"]
+        image = json.loads(div.text)['ou']
         if image.find('https:') != -1:
             images.append(image)
         

10f06df8de6cb1506914cc3bf1dbe62c82a83da3
abomyi <abomyi@gmail.com>
2017-09-13 13:08:51 +0800

    fix populate error

--- yiBot/lineBot/admin.py
+++ yiBot/lineBot/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from lineBot.models import LineUser
+from lineBot.models import LineUser, Card
 
 
 class LineUserAdmin(admin.ModelAdmin):
     list_display = ['name', 'chatFrom', 'lineID', 'commander']
     list_filter = ['chatFrom']
 
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = ['name', 'weight']
+
+
 # Register your models here.
 admin.site.register(LineUser, LineUserAdmin)
+admin.site.register(Card, CardAdmin)
--- yiBot/populate/card.py
+++ yiBot/populate/card.py
@@ -1,12 +1,9 @@
 import os
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yiBot.settings')
 import django
+django.setup()
 
 from lineBot.models import Card
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yibot.settings')
-django.setup()
 
 
 cardList = [('6星 龍拳少年 ‧ 椎拳崇', 16.89), ('6星 烈陽赤炎 ‧ 草薙京', 5.81),
@@ -15,7 +12,9 @@ cardList = [('6星 龍拳少年 ‧ 椎拳崇', 16.89), ('6星 烈陽赤炎 ‧ 
             ('6星 餓狼狂嚎 ‧ 泰利波格', 17.08), ('6星 超能女高中生 ‧ 麻宮雅典娜', 18.82)]
 
 def populate():
-    for name, weight in zip(*cardList):
+    name, weight = zip(*cardList)
+    for name, weight in zip(name, weight):
+#    for name, weight in zip(*cardList):
         Card.objects.create(name=name, weight=weight)
 
     print('卡片新增完畢')
